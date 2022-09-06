c49cb31bc3ed25c3b3998d5fde0798e165e53dca
Jo Cheung Cheung <112573606+Jwong12345@users.noreply.github.com>
2022-09-06 21:52:08 +0000

    I completed Ch1 exercise

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,18 @@
+print(10 * 5)
+print(10 ** 2)
+print(15 /10)
+print( 15// 10)
+print(-15 // 10)
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+print(10/15)
+# last answer stops after a certain amount of 6's rather then repeating it indefinately
+
+
+rate = float( input("Please input current exchange rate for the Euro to USD: ") )
+amount = float( input("Please input amount of Euro you want to exchange to USD: ") )
+total = (amount * rate)
+result = (total-3)
+print(result)
