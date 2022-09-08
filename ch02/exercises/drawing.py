906cfed579009571e318478512ebc0802651263a
Jo Cheung Wong <112573606+Jwong12345@users.noreply.github.com>
2022-09-08 14:45:59 +0000

    made a 2rn generator similar to Fire Emblem

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -5,10 +5,10 @@ myturtle = turtle.Turtle()
 myturtle.shape("turtle")
 myturtle.color("blue")
 
-length = float( input("Please input how long each side should be: "))
-num_sides = float( input("Please input how many sides you want: "))
+length = int( input("Please input how long each side should be: ") )
+num_sides = int( input("Please input how many sides you want: ") )
 angle = 360 / num_sides
-for i in [angle]*num_sides
+for i in [angle]*num_sides:
   myturtle.forward(length)
   myturtle.left(i)
 window.exitonclick()
