0441a4f93280074e6aaa7f8a9d0bfd9b658f13d7
Jo Cheung Wong <112573606+Jwong12345@users.noreply.github.com>
2022-09-19 21:29:59 +0000

    finished exercise to cut off when correct.

--- ch03/exercises/main.py
+++ ch03/exercises/main.py
@@ -1,11 +1,14 @@
 import random
 my_list = [1,2,3,4,5,6,7,8,9,10]
 num = random.choice(my_list)
+guess = 0
 for i in range(3):
+  guess = guess + 1
   ans = int(input("Please choose a number between 1 and 10: "))
   if ans > num:
-    print("too low")
-  if ans < num:
     print("too much")
+  if ans < num:
+    print("too low")
   if ans == num:
-    print("You are correct")
+    print("You are correct.", "You got it in", guess, "tries")
+    break
