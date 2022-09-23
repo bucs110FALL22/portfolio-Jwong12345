93e693b2c7a4d9657309cffcd77f3944286b300e
Jo Cheung Wong <112573606+Jwong12345@users.noreply.github.com>
2022-09-23 15:54:42 +0000

    Finished lab 3

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,7 +1,8 @@
 import turtle #1. import modules
 import random
-
-#Part A
+import pygame
+import math
+# #Part A
 window = turtle.Screen() # 2.  Create a screen
 window.bgcolor('lightblue')
 
@@ -18,9 +19,85 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
-
-
-# PART B - complete part B here
-
-
+michelangelo.forward(random.choice(range(1,101)))
+leonardo.forward(random.choice(range(1,101)))
+pygame.time.wait(5000)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+for i in range(10):
+  michelangelo.forward(random.choice(range(1,11)))
+  leonardo.forward(random.choice(range(1,11)))
+  pygame.time.wait(1000)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 window.exitonclick()
+# PART B - complete part B here
+pygame.init()
+window = pygame.display.set_mode()
+coords = []
+num_side=3
+side_length=100
+offset=100
+color = "blue"
+for a in range(num_side):
+  theta=(2*math.pi*a/num_side)
+  x=(side_length*math.cos(theta)+offset)
+  y=(side_length*math.sin(theta)+offset)
+  coords.append([x,y])
+pygame.draw.polygon(window, color, coords)
+pygame.display.flip()
+pygame.time.wait(500)
+window.fill("black")
+pygame.display.flip()
+
+num_side=4
+coords = []
+for b in range(num_side):
+  theta=(2*math.pi*b/num_side)
+  x=(side_length*math.cos(theta)+offset)
+  y=(side_length*math.sin(theta)+offset)
+  coords.append([x,y])
+pygame.draw.polygon(window, color, coords)
+pygame.display.flip()
+pygame.time.wait(500)
+window.fill("black")
+pygame.display.flip()
+
+num_side=6
+coords = []
+for c in range(num_side):
+  theta=(2*math.pi*c/num_side)
+  x=(side_length*math.cos(theta)+offset)
+  y=(side_length*math.sin(theta)+offset)
+  coords.append([x,y])
+pygame.draw.polygon(window, color, coords)
+pygame.display.flip()
+pygame.time.wait(500)
+window.fill("black")
+pygame.display.flip()
+
+num_side=9
+coords = []
+for d in range(num_side):
+  theta=(2*math.pi*d/num_side)
+  x=(side_length*math.cos(theta)+offset)
+  y=(side_length*math.sin(theta)+offset)
+  coords.append([x,y])
+pygame.draw.polygon(window, color, coords)
+pygame.display.flip()
+pygame.time.wait(500)
+window.fill("black")
+pygame.display.flip()
+
+num_side=360
+coords = []
+for e in range(num_side):
+  theta=(2*math.pi*e/num_side)
+  x=(side_length*math.cos(theta)+offset)
+  y=(side_length*math.sin(theta)+offset)
+  coords.append([x,y])
+pygame.draw.polygon(window, color, coords)
+pygame.display.flip()
+pygame.time.wait(500)
+window.fill("black")
+pygame.display.flip()
