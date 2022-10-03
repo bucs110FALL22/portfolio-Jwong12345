c6581499379af46a9711e56f5ba68c841d8f6d7e
Jo Cheung Wong <112573606+Jwong12345@users.noreply.github.com>
2022-10-03 22:07:02 +0000

    half done class exercise

---
+++ ch04/exercises/main.py
@@ -0,0 +1,26 @@
+import turtle
+import random
+
+
+window = turtle.Screen()
+mike = turtle.Turtle()
+mike.shape("turtle")
+mike.color("blue")
+
+mike.up()
+i = True
+while i is True:
+  list = [1,2]
+  choice = random.choice(list)
+  POS = []
+  POS = mike.pos()
+  if 
+  if choice == 1:
+    mike.left(90)
+    mike.forward(50)
+
+  if choice == 2:
+    mike.right(90)
+    mike.forward(50)
+
+window.exitonclick()
