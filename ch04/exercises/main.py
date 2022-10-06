d63b0243635a485ea9cec048ef195e7e159a96ad
Jo Cheung Wong <112573606+Jwong12345@users.noreply.github.com>
2022-10-06 00:31:03 +0000

    Finished exercise coin flip

--- ch04/exercises/main.py
+++ ch04/exercises/main.py
@@ -3,18 +3,31 @@ import random
 
 
 window = turtle.Screen()
+window.setup (width=400, height=300, startx=0, starty=0)
 mike = turtle.Turtle()
 mike.shape("turtle")
 mike.color("blue")
-
+mike.goto(0, 0)
 mike.up()
 i = True
 while i is True:
   list = [1,2]
   choice = random.choice(list)
+  print(choice)
   POS = []
   POS = mike.pos()
-  if 
+  x = mike.xcor()
+  y = mike.ycor()
+  print(x)
+  print(y)
+  if x > 300:
+    break
+  if x < -200:
+    break
+  if y < -200:
+    break
+  if y > 300:
+    break
   if choice == 1:
     mike.left(90)
     mike.forward(50)
